137236fb627246147973d3a92e35f66eb2429281
gtprettyman <92059089+gtprettyman@users.noreply.github.com>
2021-10-12 18:51:40 -0400

    Add files via upload

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -1,69 +1,72 @@
-#include "StringData.h"
 #include <iostream>
-#include <string>
+#include "StringData.h"
 
-    long linearSearch(std::vector<std::string> direct, std::string element) {
-        for (int index = 0; index < direct.size(); index++) {
-            if (direct[index] == element) {
-                return index;
-            }
+int linearSearch(std::vector<std::string> dataSet, std::string element) {
+
+    for(int i = 0; i < dataSet.size(); i++) {
+        if(element.compare(dataSet[i]) == 0) {
+            return i;
         }
-        return -1;
     }
 
-    long binarySearch(std::vector<std::string> direct, std::string element) {
-        int min = 0;
-        int max = direct.size()-1;
-        while (min <= max) {
-            int middle = (min + max) / 2;
-            int difference = element.compare(direct[middle]);
-            if (difference < 0) {
-                max = middle - 1;
-            } else if (difference > 0) {
-                min = middle + 1;
-            } else {
-                return middle;
-            }
+    return -1;
+}
+
+int binarySearch(std::vector<std::string> dataSet, std::string element) {
+    int max = dataSet.size() - 1;
+    int min = 0;
+    int mid = min + (max- min) / 2;
+
+    while(min <= max) {
+        if(element.compare(dataSet[mid]) == 0) {
+            return mid;
+        } else if(element.compare(dataSet[mid]) < 0) {
+            max = mid - 1;
+            mid = min + (max - min) / 2;
+        } else {
+            min = mid + 1;
+            mid = min + (max - min) / 2;
         }
-        return -1;
     }
 
-    int main() {
-        std::vector<std::string> direct = getStringData();
-
-        int index;
-
-        auto time1 = systemTimeNanoseconds();
-        index = linearSearch(direct, "not here");
-        auto time2 = systemTimeNanoseconds();
-        auto difference = time2 - time1;
-        std::cout << "Linear search found not here at " << index << " in " << difference<< std::endl;
-        time1=systemTimeNanoseconds();
-        index= binarySearch(direct, "not here");
-        time2 = systemTimeNanoseconds();
-        difference = time2-time1;
-        std::cout <<"Binary search found not here at " << index << " in " << difference << std::endl;
-        time1 = systemTimeNanoseconds();
-        index = linearSearch(direct, "mzzzz");
-        time2 = systemTimeNanoseconds();
-        difference = time2 - time1;
-        std::cout << "Linear search found mzzzz at " << index << " in " << difference << std::endl;
-        time1 = systemTimeNanoseconds();
-        index = binarySearch(direct, "mzzzz");
-        time2 = systemTimeNanoseconds();
-        difference = time2 - time1;
-        std::cout << "Binary search found mzzzz at " << index << " in " << difference << std::endl;
-        time1 = systemTimeNanoseconds();
-        index = linearSearch(direct, "aaaaa");
-        time2 = systemTimeNanoseconds();
-        difference = time2 - time1;
-        std::cout << "Linear search found aaaaa at " << index << " in " << difference << std::endl;
-        time1 = systemTimeNanoseconds();
-        index = binarySearch(direct, "aaaaa");
-        time2 = systemTimeNanoseconds();
-        difference = time2 - time1;
-        std::cout << "Binary search found aaaaa at " << index << " in " << difference << std::endl;
-        return 0;
-    }
+    return -1;
+}
 
 
+int main() {
+
+    long long startTime = 0;
+    long long endTime = 0;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of not_here found by linear search: " << linearSearch( getStringData(), "not_here") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Linear search time " << endTime - startTime << std::endl;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of not_here found by binary search: " << binarySearch(getStringData(), "not_here") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Binary search time " << endTime - startTime << std::endl;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of aaaaa found by linear search: " << linearSearch( getStringData(), "aaaaa") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Linear search time " << endTime - startTime << std::endl;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of aaaaa found by binary search: " << binarySearch(getStringData(), "aaaaa") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Binary search time " << endTime - startTime << std::endl;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of mzzzz found by linear search: " << linearSearch( getStringData(), "mzzzz") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Linear search time " << endTime - startTime << std::endl;
+
+    startTime = systemTimeNanoseconds();
+    std::cout << "Index of mzzzz found by binary search: " << binarySearch(getStringData(), "mzzzz") << std::endl;
+    endTime = systemTimeNanoseconds();
+    std::cout << "Binary search time " << endTime - startTime << std::endl;
+
+    return 0;
+}
